6516849e8d06345c626c84a3e52b90939901066f
Nasir6276 <ni3262019@gmail.com>
2023-09-01 11:05:38 +0100

    slider start

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { styled } from 'styled-components'
 import ArrowLeftOutlinedIcon from '@material-ui/icons/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@material-ui/icons/ArrowRightOutlined';
+import Vision from '../img/Orange.png'
 
 const Container = styled.div`
     width: 100%;
-    height: 100%;
-    display: flex;
+    height: 100vh;
+    display: flex;v
+    position: relative;
+    overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -14,15 +17,105 @@ const Arrow = styled.div`
     height: 50px;
     background-color: #fff7f7;
     border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props=> props.direction === "left" && "10px"};
+    right: ${props=> props.direction === "right" && "10px"};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
 `
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+`
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`
+
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Image = styled.img`
+    height: 80%;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-sixe: 70px;
+`;
+
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight:500;
+    letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`;
 
 const Slider = () => {
   return (
     <Container>
-        <Arrow>
+        <Arrow direction="left" onClick={() => {handleClick('left')}}>
             <ArrowLeftOutlinedIcon />
         </Arrow>
-        <Arrow>
+        <Wrapper>
+            <Slide>
+                <ImgContainer>
+                    <Image src={Vision} />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>SUMMER SALE</Title>
+                    <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+            </Slide>
+            <Slide>
+                <ImgContainer>
+                    <Image src={Vision} />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>SUMMER SALE</Title>
+                    <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+            </Slide>
+            <Slide>
+                <ImgContainer>
+                    <Image src={Vision} />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>SUMMER SALE</Title>
+                    <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+            </Slide>
+        </Wrapper>
+        <Arrow direction="right" onClick={() => {handleClick('right')}}>
             <ArrowRightOutlinedIcon />
         </Arrow>
     </Container>
